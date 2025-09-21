abfaf120d7433481183bcc4a60a175cd6fd5cf62
Simerjeet singh <daljeetkaur9570@gmail.com>
2025-09-21 11:26:16 +0530

    y

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,4 +1,3 @@
- import React, { useState } from 'react';
 import React, { useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
 import { useNavigate } from 'react-router-dom';
